dc4d79a2941ac7ecf4e50fc4a1bfac55171119be
Hasan Mahmud <hm791644@gmail.com>
2025-09-09 19:18:56 +0600

    2nd commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const allPlantsContainer = document.getElementById("allPlantsContainer");
 const categoriesContainer = document.getElementById("categoriesContainer");
+const cartContainer = document.getElementById("cartContainer");
 
-
+let addToCart = [];
 const loadAllPlants = async () => {
   try {
     const res = await fetch(`https://openapi.programming-hero.com/api/plants`);
@@ -15,14 +16,14 @@ const loadAllPlants = async () => {
 const showAllPlants = (plants) => {
   // console.log(plants);
   allPlantsContainer.innerHTML = "";
-plants.forEach(plant => {
-  allPlantsContainer.innerHTML += `
+  plants.forEach((plant) => {
+    allPlantsContainer.innerHTML += `
     <div class="plant-card bg-white shadow-md rounded-xl p-4 flex flex-col h-[420px] ">
       <img src="${plant.image}" 
            alt="" 
            class="w-full h-42 object-cover rounded-lg" />
       
-      <h1 class="text-xl font-semibold mt-2">${plant.name}</h1>
+      <h1 onclick="loadPlantsInfo(${plant.id})" id="${plant.id}" class="text-xl font-semibold mt-2 cursor-pointer">${plant.name}</h1>
       <p class="text-gray-600 text-sm flex-grow overflow-hidden">
         ${plant.description}
       </p>
@@ -34,21 +35,22 @@ plants.forEach(plant => {
           <i class="fa-solid fa-bangladeshi-taka-sign"></i>${plant.price}
         </h2>
       </div>
-      <button class="btn btn-wide text-white bg-green-500 rounded-2xl flex items-center mx-auto">Add to Cart</button>
+      <button class="buyButton btn btn-wide text-white bg-green-500 rounded-2xl flex items-center mx-auto">Add to Cart</button>
     </div>
   `;
-});
+  });
 
+  loadPlantsInfo();
 };
 
 loadAllPlants();
 
-
-
 // Show Categories
 const loadCategories = async () => {
   try {
-    const res = await fetch(`https://openapi.programming-hero.com/api/categories`);
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/categories`
+    );
     const data = await res.json();
     showAllCategories(data.categories);
   } catch (error) {
@@ -59,7 +61,7 @@ const loadCategories = async () => {
 const showAllCategories = (categories) => {
   categoriesContainer.innerHTML = "";
 
-  categories.forEach(category => {
+  categories.forEach((category) => {
     // console.log(category);
     // console.log(category.id);
     categoriesContainer.innerHTML += `
@@ -67,10 +69,7 @@ const showAllCategories = (categories) => {
         ${category.category_name}
       </li>
     `;
-
-
   });
-  
 };
 
 loadCategories();
@@ -78,20 +77,89 @@ loadCategories();
 // Get plant by category
 const loadByCategory = async (categoryId) => {
   try {
-    const res = await fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`);
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/category/${categoryId}`
+    );
     const data = await res.json();
     showAllPlants(data.plants);
 
-    
-    document.querySelectorAll(".category-item").forEach(item => {
+    document.querySelectorAll(".category-item").forEach((item) => {
       item.classList.remove("bg-green-600", "text-white");
     });
     const activeCategory = document.getElementById(categoryId);
     activeCategory.classList.add("bg-green-600", "text-white");
-
   } catch (error) {
     console.log(error);
   }
+};
+
+// For Modal
+
+const loadPlantsInfo = async (plantId) => {
+  try {
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/plant/${plantId}`
+    );
+    const data = await res.json();
+
+    // console.log(data)
+    showPlantsInfo(data.plants);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const showPlantsInfo = (plant) => {
+  document.getElementById("modal-title").innerText = plant.name;
+  document.getElementById("modal-image").src = plant.image;
+  document.getElementById("modal-description").innerText = plant.description;
+  document.getElementById("modal-category").innerText = plant.category;
+  document.getElementById("modal-price").innerHTML = `
+    <i class="fa-solid fa-bangladeshi-taka-sign"></i>${plant.price}
+  `;
+  document.getElementById("my_modal_5").showModal();
+};
+
+loadPlantsInfo();
+
+const handleCart = (e) => {
+   const card = e.target.parentNode;
+  const plantTitle = card.children[1].innerText;
+  const plantPrice = card.children[3].children[1].innerText; 
+  
+  const current = addToCart.find(item => item.title === plantTitle);
+
+  if (current) {
+    current.quantity += 1;
+  } else {
+    addToCart.push({
+      title: plantTitle,
+      price: plantPrice,
+      quantity: 1
+    });
+  }
+
+  showCart(addToCart);
+};
+
+
+allPlantsContainer.addEventListener('click', (e) => {
+  if (e.target.innerText === "Add to Cart") {
+    handleCart(e);
+  }
+});
+
+const showCart = (addToCart) => {
+  cartContainer.innerHTML = ""; 
+
+  addToCart.forEach(cart => {
+    cartContainer.innerHTML += `
+      <li class="flex justify-between items-center border-b py-2">
+        <span class="font-medium">${cart.title}</span>
+        <span class="font-bold text-green-600">${cart.price} x ${cart.quantity}</span>
+      </li>
+    `;
+  });
 };
 
 
